d422348a2d3cca8e5739624583642653d9246e11
c0664020 <c0664020@SB-N208-06.lambton.on.ca>
2015-09-14 15:49:14 -0400

    initial commit( answered the question)

---
+++ src/javaassignment1/pkg2015/JavaAssignment12015.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment1.pkg2015;
+
+/**
+ *
+ * @author c0664020
+ */
+public class JavaAssignment12015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // System.out.println("Hello World!");
+        
+        /* ***************************
+        *Assignment #1
+        *Job title: Jr.Java Developer
+        *http://www.indeed.ca/cmp/Inspired-Staffing-Inc./jobs/Junior-Java-Developer-33c75cda53a56495?q=java+developer
+        *
+        *
+        *Java EE: Java Enterprise Edition - https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+        *
+        *Toronto : $80k
+        *Vancouver: $85k
+        *waterloo : $80k
+        */
+        
+    }
+    
+}
